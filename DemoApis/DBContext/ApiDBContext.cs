fb894074b22ad6a335303fa2c007f6fa4208376e
Rohit Yadav <ry0513169@gmail.com>
2025-08-04 21:16:05 +0530

    Add project files.

---
+++ DemoApis/DBContext/ApiDBContext.cs
@@ -0,0 +1,12 @@
+﻿using DemoApis.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoApis.DBContext
+{
+    public class ApiDBContext : DbContext
+    {
+        public ApiDBContext(DbContextOptions<ApiDBContext> options) : base(options) { 
+        }
+
+    }
+}
